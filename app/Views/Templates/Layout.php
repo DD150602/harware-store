e57e6a2c6554aa02796d99e4f08ab634c1f1acbd
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-10-09 12:18:22 -0500

    chore(Configuration): :wrench: Change scripts and scripts sections added

--- app/Views/Templates/Layout.php
+++ app/Views/Templates/Layout.php
@@ -6,10 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="<?= base_url('resources/css/bootstrap.min.css') ?>">
-  <link rel="stylesheet" href="<?= base_url('resources/css/sweetalert.min.css') ?>">
-  <script defer type="module" src="<?= base_url('resources/js/bootstrap.bundle.min.js') ?>"></script>
-  <script defer type="module" src="<?= base_url('resources/js/popper.min.js') ?>"></script>
-  <script defer type="module" src="<?= base_url('resources/js/sweetalert.all.min.js') ?>"></script>
+  <link rel="stylesheet" href="<?= base_url('resources/css/sweetalert2.min.css') ?>">
+  <script defer src="<?= base_url('resources/js/bootstrap.bundle.min.js') ?>"></script>
+  <script defer src="<?= base_url('resources/js/popper.min.js') ?>"></script>
+  <script defer src="<?= base_url('resources/js/sweetalert2.all.min.js') ?>"></script>
+
+  <?= $this->renderSection('scripts') ?>
 </head>
 
 <body>
