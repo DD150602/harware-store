d590a0d4dee739fdde8cf070c266ce0815edfe13
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-10-15 11:26:58 -0500

    style(Views): :lipstick: Add custome css file

--- app/Views/Templates/Layout.php
+++ app/Views/Templates/Layout.php
@@ -7,6 +7,7 @@
   <title>Document</title>
   <link rel="stylesheet" href="<?= base_url('resources/css/bootstrap.min.css') ?>">
   <link rel="stylesheet" href="<?= base_url('resources/css/sweetalert2.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('resources/main.css') ?>">
   <script defer src="<?= base_url('resources/js/bootstrap.bundle.min.js') ?>"></script>
   <script defer src="<?= base_url('resources/js/popper.min.js') ?>"></script>
   <script defer src="<?= base_url('resources/js/sweetalert2.all.min.js') ?>"></script>
