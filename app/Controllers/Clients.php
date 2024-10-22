c1cd7f30b743f63c2400ee9f4a766a3176a05b4b
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-10-22 01:36:32 -0500

    feat(Bussines Logic): :sparkles: Add logic to create clients

--- app/Controllers/Clients.php
+++ app/Controllers/Clients.php
@@ -16,7 +16,43 @@ class Clients extends BaseController
   }
   public function index()
   {
-
     return view('Clients/Clients', $this->data);
+  }
+
+  public function newClient()
+  {
+    $rules = [
+      'client_name' => [
+        'rules' => 'string|required',
+        'errors' => [
+          'string' => 'The client name field must contain only letters',
+          'required' => 'The client name field is required'
+        ],
+      ],
+      'client_phone' => [
+        'rules' => 'numeric|required|is_natural|is_unique[clients.client_phone]',
+        'errors' => [
+          'numeric' => 'The client phone field must contain only numbers',
+          'required' => 'The client phone field is required',
+          'is_natural' => 'The client phone field must be a natural number',
+          'is_unique' => 'The client phone is already in use'
+        ],
+      ],
+      'client_address' => [
+        'rules' => 'string',
+        'errors' => ['string' => 'The client address field must contain only letters'],
+      ],
+    ];
+
+    $dataToValidate = $this->request->getPost(array_keys($rules));
+    if (!$this->validateData($dataToValidate, $rules)) {
+      return redirect()->back()->with('message', 2)->withInput();
+    } else {
+      $data = $this->request->getPost();
+      $data['client_created_by'] = session('login_info')['user_id'];
+      // print_r($data);
+      $response = $this->client->createClient($data);
+      return redirect()->back()->with('message', $response);
+    }
   }
 }
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -11,7 +11,7 @@ class Client extends Model
   protected $useAutoIncrement = true;
   protected $returnType = 'object';
   protected $useSoftDeletes = true;
-  protected $allowedFields = ['client_id', 'client_name', 'client_phone', 'client_address'];
+  protected $allowedFields = ['client_id', 'client_name', 'client_phone', 'client_address', 'client_created_by'];
   protected $useTimestamps = true;
   protected $dateFormat = 'datetime';
   protected $createdField = 'client_created_at';
@@ -27,5 +27,15 @@ class Client extends Model
   {
     return $this->select('client_id, client_name, client_phone, client_address')
       ->where('client_phone', $data['client_phone'])->first();
+  }
+
+  public function createClient(array $data): int
+  {
+    $insert = $this->insert($data);
+    if ($insert) {
+      return 1;
+    } else {
+      return 2;
+    }
   }
 }
