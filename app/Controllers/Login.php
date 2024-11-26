18201685f3a6a00406e17cb44d20e0bbdcb608b9
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-11-26 11:23:28 -0500

    docs(Controllers): :memo: Add Docstring for the login's method controller

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -8,15 +8,40 @@ class Login extends BaseController
 {
   protected $user;
 
+  /**
+   * Constructor
+   *
+   * @return void
+   */
   public function __construct()
   {
     $this->user = new User();
   }
+
+  /**
+   * Displays the login page.
+   *
+   * @return string The rendered login view.
+   */
   public function index(): string
   {
     return view('Login');
   }
-
+  /**
+   * Handle user login process.
+   *
+   * This method validates the login form input and processes the login request.
+   * If validation fails, the user is redirected back with the entered input.
+   * If the login is successful, user session data is set and the user is redirected
+   * to the Products page. Otherwise, the user is redirected back to the login page
+   * with an error message.
+   *
+   * - Redirects back to the login form with input data if validation fails.
+   * - Redirects to the Products page if login is successful.
+   * - Redirects to the login page with an error message if login fails.
+   *
+   * @return \CodeIgniter\HTTP\RedirectResponse Redirects to the appropriate page based on the login result.
+   */
   public function login()
   {
     $rules = [
@@ -54,6 +79,13 @@ class Login extends BaseController
     }
   }
 
+  /**
+   * Logs out the current user by destroying the session.
+   *
+   * This method terminates the user session and redirects to the login page.
+   *
+   * @return \CodeIgniter\HTTP\RedirectResponse Redirects to the login page.
+   */
   public function logout()
   {
     $this->session->destroy();
