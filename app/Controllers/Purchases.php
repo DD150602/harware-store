1b026945d9703b297fa852d96546968258deb9b7
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-11-26 11:40:21 -0500

    docs(Controllers): :memo: Add Docstring for the purchase's method controller

--- app/Controllers/Purchases.php
+++ app/Controllers/Purchases.php
@@ -17,6 +17,13 @@ class Purchases extends BaseController
   protected $categories;
   protected $data = [];
 
+  /**
+   * Constructor for the Controller.
+   *
+   * Initializes the required models and retrieves data for the `purchases` property.
+   * These models are used throughout the controller to handle operations for purchases, 
+   * purchase details, suppliers, products, and categories.
+   */
   public function __construct()
   {
     $this->purchase = new Purchase();
@@ -27,23 +34,60 @@ class Purchases extends BaseController
     $this->data['purchases'] = $this->purchase->getAllPurchases();
   }
 
+  /**
+   * Displays the main Purchases page.
+   *
+   * This method loads the `Purchases/Purchases` view and passes data to it. 
+   * The data includes information such as all purchases retrieved in the constructor.
+   *
+   * @return \CodeIgniter\HTTP\ResponseInterface The rendered view for the Purchases page.
+   */
   public function index()
   {
     return view('Purchases/Purchases', $this->data);
   }
 
+  /**
+   * Displays the Create Purchase page.
+   *
+   * This method loads the `Purchases/CreatePurchase` view, which contains a form
+   * for creating a new purchase. This form includes fields for selecting a supplier
+   * and adding multiple products to the purchase.
+   *
+   * @return \CodeIgniter\HTTP\ResponseInterface The rendered view for the Create
+   * Purchase page.
+   */
   public function createView()
   {
     return view('Purchases/CreatePurchase');
   }
 
-  public function purchase($id)
+  /**
+   * Displays the details of a specific purchase.
+   *
+   * This method retrieves the details of a specific purchase using the provided purchase ID.
+   * It fetches the purchase data and its associated purchase details and passes them to the view.
+   *
+   * @param int $id The ID of the purchase to retrieve.
+   * 
+   * @return \CodeIgniter\HTTP\ResponseInterface The rendered view displaying the purchase details.
+   */
+  public function purchase(int $id)
   {
     $this->data['purchase'] = $this->purchase->getPurchase($id);
     $this->data['purchase_details'] = $this->purchaseDetails->getPurchaseDetails($id);
     return view('Purchases/PurchaseDetails', $this->data);
   }
 
+  /**
+   * Handles the supplier information retrieval for a purchase.
+   *
+   * This method retrieves supplier information based on the POST data from the request.
+   * If the supplier information is found, it also fetches all categories and products 
+   * for creating a purchase. The data is then passed to the "Create Purchase" view.
+   *
+   * @return \CodeIgniter\HTTP\ResponseInterface The rendered view for creating a purchase.
+   */
   public function supplierInfo()
   {
     $dataPost = $this->request->getPost();
@@ -57,6 +101,24 @@ class Purchases extends BaseController
     return view('Purchases/CreatePurchase', $this->data);
   }
 
+  /**
+   * Handles the creation of a purchase and its related details.
+   *
+   * This method processes a JSON payload containing purchase and product details, 
+   * and performs the following steps:
+   * - Creates a new purchase record in the database.
+   * - Verifies if each product in the purchase exists in the database:
+   *   - If the product doesn't exist, a new product record is created.
+   *   - If the product exists, its stock is updated by adding the purchased quantity.
+   * - Associates the products with the purchase by creating purchase details.
+   * - Updates the product stock in the database.
+   * - Inserts the purchase details into the database in a batch operation.
+   *
+   * If the batch insertion of purchase details is successful, it returns a JSON response
+   * with a success message and the list of products. Otherwise, it returns an error response.
+   *
+   * @return \CodeIgniter\HTTP\Response JSON response indicating the success or failure of the operation.
+   */
   public function create()
   {
     $dataPost = $this->request->getJSON();
