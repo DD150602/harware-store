1d3bd2f28ba6a17318741696ef61f3c76a47ca6b
DD150602 <ljmdylan.delgado1011@gmail.com>
2024-12-04 15:23:13 -0500

    feat(Bussiness Logic): :sparkles: Add functionality for crating categories

--- app/Models/CategoryModel.php
+++ app/Models/CategoryModel.php
@@ -11,7 +11,7 @@ class CategoryModel extends Model
   protected $useAutoIncrement = true;
   protected $returnType = 'object';
   protected $useSoftDeletes = true;
-  protected $allowedFields = ['category_id', 'category_name'];
+  protected $allowedFields = ['category_id', 'category_name', 'category_created_by'];
   protected $useTimestamps = true;
   protected $dateFormat = 'datetime';
   protected $createdField = 'category_created_at';
@@ -27,5 +27,25 @@ class CategoryModel extends Model
   public function getCategories()
   {
     return $this->findAll();
+  }
+
+  /**
+   * Creates a new category.
+   *
+   * This method takes an array of category data to create a new category in the
+   * database. If the category is successfully created, it returns a status code of 1.
+   * If the category fails to be inserted, it returns a status code of 2.
+   *
+   * @param array $data An array containing category data, such as the name and
+   *                    creator's user ID.
+   *
+   * @return int Returns a status code for the category creation attempt.
+   */
+  public function newCategory(array $data): int
+  {
+    if ($this->insert($data)) {
+      return 1;
+    }
+    return 2;
   }
 }
